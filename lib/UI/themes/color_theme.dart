8ec39adb6bd56e4a5f695ba79fdbee417de0303b
Ribesh Basnet <90415424+ribesh-code@users.noreply.github.com>
2022-07-26 11:42:34 +0545

    feat: initial starter code.

---
+++ lib/UI/themes/color_theme.dart
@@ -0,0 +1,9 @@
+class ColorTheme {
+  ColorTheme._();
+
+  static const int primary = 0xFF013856;
+  static const int primaryDark = 0xFF061735;
+  static const int grayDark = 0xFF5D7989;
+  static const int secondary = 0xFF1FA4B9;
+  static const int background = 0xFFF8FCFF;
+}
